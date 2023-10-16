9d81d92172736efdefd19656b2bde8f4dac7c5f6
caiogodoyy <caio.godoy.vg@gmail.com>
2023-10-16 16:02:30 -0300

    creating spring project

---
+++ src/test/java/com/caio/rinha/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.caio.rinha.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
